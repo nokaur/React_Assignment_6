c794d108dded3009002f5ab54dd512683293cefc
nokaur <71750548+nokaur@users.noreply.github.com>
2020-10-01 17:43:06 +0530

    Changes-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import Route from "react-router-dom/Route";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="link">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/projects">Project</Link>
+              </li>
+            </ul>
+          </div>
+          <Route
+            path="/"
+            exact
+            strict
+            render={() => {
+              return <h1>You Chose Home</h1>;
+            }}
+          />
+
+          <Route
+            path="/projects"
+            exact
+            strict
+            render={() => {
+              return <h1>You Chose Project</h1>;
+            }}
+          />
+        </div>
+      </Router>
+    );
+  }
 }
+
+export default App;
